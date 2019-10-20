0401e7001c9f9a446c51ea6148b2ca50661c46d0
Elie Oriol <elie.oriol@polytechnique.edu>
2019-10-20 21:07:56 +0200

    TD4 corrigé partiel

--- TD4/TD4.py
+++ TD4/TD4.py
@@ -22,3 +22,122 @@ print("Insertion :", T)
 T = [9,3,12,5,1]
 triRapideEnPlace(T)
 print("Rapide (en place) :", T)
+
+# 3.4
+T = [9,3,12,5,1]
+T = triRapide(T)
+print("Rapide :", T)
+
+# 3.5
+import time
+import random as rd
+
+for size in [1000, 10000, 100000]:
+    l=[]
+    for i in range(size):
+        l.append(rd.randint(-50, 50))
+    tps = time.time()
+    triRapide(l)
+    print('Tri rapide (taille '+str(size)+') :',  time.time()-tps)
+
+
+## Exercice 4 : Tri Fusion
+
+T = [9,3,12,5,1]
+T = triFusion(T)
+print("Fusion :", T)
+
+
+## Exercice 5 : Tri à Bulles
+
+# 5.2
+T = [9,3,12,5,1]
+triBulles(T)
+print("Bulles :", T)
+
+# 5.3
+T = [9,3,12,5,1]
+triBullesOpti(T)
+print("Bulles opti :", T)
+
+
+## Exercice 6 : Comparaison
+
+# 6.1
+import numpy as np
+
+def listeAlea(n):
+    l=[]
+    for i in range(n):
+        l.append(rd.randint(-1000, 1000))
+    return l
+    # une possibilite plus rapide avec numpy :
+    # return np.random.randint(-1000, 1000, size=n)
+
+
+# 6.2
+fich = open("tri_results.txt", 'w')
+fich.write("Taille\tTB\tTS\tTI\tTR\tTF\tPS\n")
+
+result = np.zeros((6,10))
+cpt = 0
+for n in range(1000, 10001, 1000):
+    print(n, end=" ")
+    fich.write(str(n))
+    tps = np.zeros(6)
+
+    # on repete 10 fois, on ajoute les temps de chaque algo de tri
+    # on passe a chaque fois en argument l[:] pour copier l, car sinon on va trier la liste une premiere fois avec triSelection() puis appeler les autres fonctions sur cette liste triee
+    for i in range(10):
+        l = listeAlea(n)
+
+        t0 = time.time()
+        triBullesOpti(l[:])
+        tps[0] += time.time()-t0
+
+        t0 = time.time()
+        triSelection(l[:])
+        tps[1] += time.time()-t0
+
+        t0 = time.time()
+        triInsertion(l[:])
+        tps[2] += time.time()-t0
+
+        t0 = time.time()
+        triRapide(l[:])
+        tps[3] += time.time()-t0
+
+        t0 = time.time()
+        triFusion(l[:])
+        tps[4] += time.time()-t0
+
+        t0 = time.time()
+        l[:].sort()
+        tps[5] += time.time()-t0
+
+    # on moyenne les temps sur les 10 repetitions et on ecrit les resultats dans le fichier
+    for k in range(6):
+        result[k,cpt] = tps[k]/10
+        fich.write("\t"+str(round(result[k,cpt], 4)))
+    fich.write("\n")
+    cpt += 1
+fich.close()
+
+
+# 6.3
+import matplotlib.pyplot as plt
+
+x = range(1000, 10001, 1000)
+plt.plot(x, result[0], label="Tri bulles")
+plt.plot(x, result[1], label="Tri selection")
+plt.plot(x, result[2], label="Tri insertion")
+plt.plot(x, result[3], label="Tri rapide")
+plt.plot(x, result[4], label="Tri fusion")
+plt.plot(x, result[5], label="Python sort")
+plt.yscale('log')
+plt.legend()
+plt.title("Temps moyen d'éxécution des algorithmes de tri")
+plt.xlabel("Taille")
+plt.ylabel("Temps")
+plt.savefig('tps.jpg')
+plt.show()
--- TD4/mesFonctionsTri.py
+++ TD4/mesFonctionsTri.py
@@ -65,3 +65,95 @@ def triRapideEnPlace(L, d=0, f=-1):
     # on trie recursivement les 2 sous-listes separees par le(s) pivot(s)
     triRapideEnPlace(L, d, d+m)
     triRapideEnPlace(L, d+p, f)
+
+
+# 3.4
+import random as rd
+
+def separation(L):
+    # on choisit aleatoirement un pivot
+    indPivot = rd.randint(0, len(L)-1)
+    pivot = L[indPivot]
+
+    # on construit les listes avec les termes inferieurs, egaux et superieurs au pivot
+    moins, egal, plus = [], [], []
+    for x in L:
+        if x < pivot:
+            moins.append(x)
+        elif x == pivot:
+            egal.append(x)
+        else:
+            plus.append(x)
+    return moins, egal, plus
+
+def triRapide(L):
+    # condition d'arret : la sous-liste est de longueur 0 ou 1
+    if len(L) <= 1:
+        return L
+
+    # on recupere les trois sous-listes des elements inferieurs, egaux et superieurs a un pivot de L
+    moins, egal, plus = separation(L)
+
+    # on retourne la concatenation de ces 3 sous-listes en triant recursivement les 2 sous-listes moins et plus
+    return triRapide(moins) + egal + triRapide(plus)
+
+
+## Exercice 4 : Tri Fusion
+
+# 4.1
+def fusion(L1, L2):
+    i, j = 0, 0
+    L = []
+
+    # i et j parcourent L1 et L2 respectivement, on ajoute a L les elements de L1 et L2 dans l'ordre croissant
+    while i < len(L1) and j < len(L2):
+        if L1[i] > L2[j]:
+            L.append(L2[j])
+            j += 1
+        else:
+            L.append(L1[i])
+            i += 1
+
+    # une liste aura fini d'etre parcourue avant l'autre, on ajoute le reste de cette liste a L
+    if i == len(L1):
+        L.extend(L2[j:])
+    else:
+        L.extend(L1[i:])
+    return L
+
+
+# 4.2
+def triFusion(L):
+    # condition d'arret : la sous-liste est de longueur 0 ou 1
+    if len(L) <= 1:
+        return L
+
+    # on separe la liste en 2 sous-listes de tailles egales (a 1 pres) et on trie les 2 sous-listes
+    moitie = len(L)//2
+    L1 = triFusion(L[:moitie])
+    L2 = triFusion(L[moitie:])
+
+    # on retourne le resultat de la fusion des sous-listes triees
+    return fusion(L1, L2)
+
+
+## Exercice 5 : Tri à Bulles
+
+# 5.2
+def triBulles(L):
+    for i in range(len(L)-1, 0, -1):
+        for j in range(i):
+            if L[j+1] < L[j]:
+                L[j+1], L[j] = L[j], L[j+1]
+
+
+# 5.3
+def triBullesOpti(L):
+    for i in range(len(L)-1, 0, -1):
+        fini = True
+        for j in range(i):
+            if L[j+1] < L[j]:
+                L[j+1], L[j] = L[j], L[j+1]
+                fini = False
+        if fini:
+            return
